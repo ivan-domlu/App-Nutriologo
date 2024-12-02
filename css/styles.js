aa9335cd862470bef65cc2dfaa4eb547f1df7c20
Laudiel Vinalay Ataxca <89664484+LaudielVinalay@users.noreply.github.com>
2024-12-02 00:02:18 -0600

    Agregado Cuenta Screen
    Agregado Cuenta Screen

--- css/styles.js
+++ css/styles.js
@@ -180,7 +180,172 @@ const styles = StyleSheet.create({
       borderRadius: 8,
       paddingHorizontal: 10,
       marginBottom: 40,
+    },
+    labelRegister: {
+      width: '90%',
+      fontSize: 16,
+      marginBottom: 8,
+      color: verdeOscuro,
+      fontWeight: 'bold'
+    },
+    inputRegister: {
+      width: '90%',
+      borderBottomWidth: 1,
+      borderBottomColor: verdeOscuro,       
+      padding: 5,
+      marginBottom: 24,
+      fontSize: 16,
+    },
+    checkboxContainer: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      marginBottom: 32      
+    },
+    checkbox: {
+      fontSize: 24,
+      marginRight: 10,
+      marginBottom: 16
+    },
+    termsText: {
+      fontSize: 16,
+      color: verdeOscuro,
+      marginBottom: 16
+    },
+    imageComida: {
+      width: 200,
+      height: 200,
+      marginBottom: 16
+    },
+
+
+    //AgendaNutriologo
+    AgendaContainer: {
+      flex: 0,
+      paddingTop: 40,
+      paddingLeft: 25,
+      alignItems: 'flex-start',
+      justifyContent: 'flex-start',
+    },
+
+    calendarContainer: {
+      flex: 0,
+      padding: 0,
+      marginTop: 5,
+      justifyContent: 'flex-start',
+      alignItems: 'center',
+    },
+
+    AgendaTitle: {
+      font: 'inter',
+      fontSize: 30, 
+      fontWeight: 'semi bold', 
+      textAlign: 'center', 
+      color: '#576e40', 
+      marginBottom: 5,
+    },
+
+    Month: {
+      font: 'inter',
+      fontSize: 24, 
+      fontWeight: 'semi bold', 
+      textAlign: 'center', 
+      color: '#576e40', 
+      marginTop: 0,
+      marginBottom: 5,
+    },
+
+    //Dia seleccionado NUTRIOLOGO
+    scrollAgenda: {
+      flexGrow: 1,
+      backgroundColor: '#fff',
+    },
+    dateInfo: {
+      padding: 10,
+      marginTop: 10,
+      paddingLeft: 25,
+      alignItems: 'flex-start', 
+    },
+    dateText: {
+      fontSize: 15,
+      fontWeight: 'bold',
+      color: '#576E40',
+      marginBottom: 10,
+    },
+    PacientePrimaria: {
+      fontSize: 12,
+      paddingLeft: 25,
+      fontWeight: 'bold',
+      color: '#576E40',
+      marginBottom: 5,
+    },
+    PacienteSecundaria: {
+      fontSize: 12,
+      paddingLeft: 25,
+      color: '#bcd09f', 
+      marginBottom: 5,
+      fontStyle: 'italic', 
+    },
+
+    //AgendaPaciente
+    foodContainer: {
+      marginTop: 20,
+      paddingLeft: 25,
+      paddingRight: 20,
+    },
+    selectedDateText: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      color: '#576E40',
+      marginBottom: 10,
+    },
+    mealRow: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+    },
+    mealCard: {
+      height: 150,
+      flex: 1,
+      backgroundColor: '#BCD09F',
+      margin: 5,
+      padding: 10,
+      borderRadius: 8,
+      alignItems: 'center',
+    },
+    mealTitle: {
+      fontSize: 16,
+      fontWeight: 'bold',
+      color: '#576E40',
+      marginBottom: 5,
+    },
+    mealDescription: {
+      fontSize: 14,
+      color: '#6D6D6D',
+    },
+    mealContainer: {
+      flex: 1, 
+      justifyContent: 'center', 
+      alignItems: 'center',
+    },
+    profileContainer: {
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginVertical: 40,
+    },
+    profileImage: {
+      width: 120,
+      height: 120,
+      borderRadius: 60,
+    },
+    button: {
+      marginVertical: 10,
+      alignSelf: 'center',
+    },
+    buttonSecondary: {
+      marginVertical: 10,
+      alignSelf: 'center',
+      borderWidth: 2,
     }
+
   });
 
 export default styles;
